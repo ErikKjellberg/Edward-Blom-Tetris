93fc7d579716e59cfb236534e13b4aeabc5911ba
Erik Kjellberg <erikkjellberg@erikspc>
2020-09-22 21:28:49 +0200

    Changed file paths to images

--- createimage.py
+++ createimage.py
@@ -2,7 +2,7 @@ import pygame
 
 def create_image(image, x=0, y=0):
     if x>0 and y>0:
-        finished_image = pygame.transform.scale(pygame.image.load("C:\\Users\\erikkg\\Documents\\Dokument\\Python\\Tetris\\"+image).convert_alpha(),(x,y))
+        finished_image = pygame.transform.scale(pygame.image.load(image).convert_alpha(),(x,y))
     else:
-        finished_image = pygame.image.load("C:\\Users\\erikkg\\Documents\\Dokument\\Python\\Tetris\\"+image).convert_alpha()
+        finished_image = pygame.image.load(image).convert_alpha()
     return finished_image
